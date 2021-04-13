41fc0810cf54ca4932e3146382174d852fbd0f3a
Raunaq Arora <60340725+raunaqarora21@users.noreply.github.com>
2021-04-13 15:59:57 +0530

    Updated Node Class

--- src/myinterface/Node.java
+++ src/myinterface/Node.java
@@ -1,5 +1,40 @@
 package myinterface;
 
-public interface Node<E extends Comparable<E>> {
-    //marker interface
+public class Node<E extends Comparable<E>>{
+
+    private E data;
+    private implementation.Node<E> Leftchild;
+    private implementation.Node<E> Rightchild;
+
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public void setLeftchild(implementation.Node<E> leftchild) {
+        Leftchild = leftchild;
+    }
+
+    public implementation.Node<E> getRightchild() {
+        return Rightchild;
+    }
+
+    public void setRightchild(implementation.Node<E> rightchild) {
+        Rightchild = rightchild;
+    }
+
+    public implementation.Node<E> getLeftchild() {
+        return Leftchild;
+    }
+
+    public Node(E data) {
+        this.data = data;
+        Leftchild = null;
+        Rightchild = null;
+    }
 }
+
